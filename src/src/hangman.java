625f7a63a688c74c0d27cff32da76ed1b5c862f8
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2022-02-01 14:59:40 +0100

    Fixat några saker och Ascii-bilder

--- src/src/hangman.java
+++ src/src/hangman.java
@@ -3,21 +3,13 @@ import java.util.Arrays;
 
 public class hangman {
     public static void main(String[] args) {
-        String ord = "yxa";
+        String ord = "ananas";
         String gissning = "";
         String fel = "";
         int antal_fel = 0;
         int kvar;
-
-        char[] ord2 = new char[ord.length()];
-        for (int i = 0; i < ord.length(); i++){
-            ord2[i] = '_';
-        }
-
-        char[] ordArray = new char[ord.length()];
-        for (int i = 0; i < ord.length(); i++){
-            ordArray[i] = ord.charAt(i);
-        }
+        char[] ord2 = hideWord(ord);
+        char[] ordArray = makeArray(ord);
 
         System.out.println("Ordet är: ");
         System.out.println(ord2);
@@ -27,7 +19,12 @@ public class hangman {
             gissning = JOptionPane.showInputDialog("Bokstav?" + "(Bara en bokstav eller hela ordet)");
             char charGissning = gissning.charAt(0);
             if (ord.contains(gissning)){
-                ord2[ord.indexOf(gissning)] = charGissning;
+                for (int o = 0; o < ord.length(); o++){
+                    if (ord.charAt(o) == charGissning){
+                        ord2[o] = charGissning;
+                    }
+                }
+                //ord2[ord.indexOf(gissning)] = charGissning;
 
                 System.out.println("Gissningen är rätt");
                 if (gissning.equals(ord) || Arrays.equals(ord2,ordArray)){
@@ -41,6 +38,80 @@ public class hangman {
                     break;
                 }
             }
+
+            if (antal_fel == 1){
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+            }
+            if (antal_fel == 2){
+                System.out.println("__________");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+            }
+            if (antal_fel == 3){
+                System.out.println("__________");
+                System.out.println("|       |");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+            }
+            if (antal_fel == 4){
+                System.out.println("__________");
+                System.out.println("|       |");
+                System.out.println("|     {'_'}");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+                System.out.println("|");
+            }
+            if (antal_fel == 5){
+                System.out.println("__________");
+                System.out.println("|       |");
+                System.out.println("|     {'_'}");
+                System.out.println("|       |");
+                System.out.println("|       |");
+                System.out.println("|");
+                System.out.println("|");
+            }
+            if (antal_fel == 6){
+                System.out.println("__________");
+                System.out.println("|       |");
+                System.out.println("|     {'_'}");
+                System.out.println("|       |");
+                System.out.println("|       |");
+                System.out.println("|      | |");
+                System.out.println("|");
+            }
+            if (antal_fel == 7){
+                System.out.println("__________");
+                System.out.println("|       |");
+                System.out.println("|     {'_'}");
+                System.out.println("|      -|-");
+                System.out.println("|       |");
+                System.out.println("|      | |");
+                System.out.println("|");
+            }
+            if (antal_fel == 8){
+                System.out.println("__________");
+                System.out.println("|       |");
+                System.out.println("|     {'_'}");
+                System.out.println("|      -|-");
+                System.out.println("|       |");
+                System.out.println("|      | |");
+                System.out.println("|");
+            }
+
+
             System.out.println("Ordet är: ");
             System.out.println(ord2);
             System.out.println("Bokstäver som blivit fel: " + fel);
@@ -60,5 +131,21 @@ public class hangman {
                 System.out.println("ERROR");
             }
         }
+    }
+
+    public static char[] hideWord(String ord) {
+        char[] ord2 = new char[ord.length()];
+        for (int i = 0; i < ord.length(); i++){
+            ord2[i] = '_';
+        }
+        return ord2;
+    }
+
+    public static char[] makeArray(String ord) {
+        char[] ordArray = new char[ord.length()];
+        for (int i = 0; i < ord.length(); i++){
+            ordArray[i] = ord.charAt(i);
+        }
+        return ordArray;
     }
 }
